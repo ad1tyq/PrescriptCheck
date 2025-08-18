615ec99968fd036882a164ac40bd9b5609cb7638
ad1tyq <adityabhardwaj9b7@gmail.com>
2025-08-18 16:52:21 +0530

    added homepage and footer

---
+++ frontend/data/testimonial.js
@@ -0,0 +1,22 @@
+export const UserTestimonials = [
+    {
+        id: 1,
+        image: "../users/image\ copy\ 2.png",
+        review: "monkE",
+    },
+    {
+        id: 2,
+        image: "../users/image\ copy\ 3.png",
+        review: "boop",
+    },
+    {
+        id: 3,
+        image: "../users/image\ copy.png",
+        review: "arghhhh",
+    },
+    {
+        id: 4,
+        image: "../users/image.png",
+        review: "wand sum vodka?",
+    },
+]
--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,9 +1,12 @@
 function Navbar(){
     return(
         <>
-        <nav className="flex bg-gray-100 text-xl px-10 py-5 border-0 border-b-1">
-            <a className="text-gray-900 text-3xl" href="/">ScriptCheck</a>
-            <ul className="flex ml-204">
+        {/* navbar */}
+        <nav className="flex bg-gray-100 text-md px-10 py-5 border-0 border-b-1">
+            {/* logo */}
+            <a className="text-gray-900 text-2xl" href="/">ScriptCheck</a>
+            {/* navbar links */}
+            <ul className="flex ml-119">
                 <CustomLink href="/upload">Upload Prescription</CustomLink>
                 <CustomLink href="/analysis">Medication Analysis</CustomLink>
                 <CustomLink href="/history">Medication History</CustomLink>
@@ -16,6 +19,8 @@ function CustomLink({href, children, ...props}){
     const path = window.location.pathname;
     return(
         <>
+        {/* navbar link component */}
+        {/* if active ? css1 : css2 (for every other link) */}
         <li className={path === href ? "text-gray-900 w-[17rem] hover:underline justify-center pt-1 flex" : "hover:text-gray-900 pt-1 hover:underline text-gray-600 w-[17rem] justify-center flex"}>
             <a href={href} {...props}>{children}</a>
         </li>
