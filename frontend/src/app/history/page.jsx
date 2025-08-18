87f56d7c37cbdfa1c455f63e1ddd8ee65894934a
ad1tyq <adityabhardwaj9b7@gmail.com>
2025-08-19 00:35:16 +0530

    somefiles added

--- frontend/src/app/history/page.jsx
+++ frontend/src/app/history/page.jsx
@@ -2,7 +2,7 @@ function HistoryPage(){
     return(
         <>
         <div className="overflow-y-scroll h-[31rem]" style={{scrollbarWidth: 'none'}}>
-            <p>History</p>
+            <h1 className="text-3xl ml-10 font-bold py-5 ml-0">Medication History</h1>
         </div>
         </>
     )
--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -6,7 +6,7 @@ function Navbar(){
             {/* logo */}
             <a className="text-gray-900 text-2xl" href="/">ScriptCheck</a>
             {/* navbar links */}
-            <ul className="flex ml-119">
+            <ul className="flex ml-[clamp(5rem,47vw,47rem)] gap-10">
                 <CustomLink href="/upload">Upload Prescription</CustomLink>
                 <CustomLink href="/analysis">Medication Analysis</CustomLink>
                 <CustomLink href="/history">Medication History</CustomLink>
@@ -21,7 +21,7 @@ function CustomLink({href, children, ...props}){
         <>
         {/* navbar link component */}
         {/* if active ? css1 : css2 (for every other link) */}
-        <li className={path === href ? "text-gray-900 w-[17rem] hover:underline justify-center pt-1 flex" : "hover:text-gray-900 pt-1 hover:underline text-gray-600 w-[17rem] justify-center flex"}>
+        <li className={path === href ? "text-gray-900 w-[10rem] hover:underline justify-center pt-1 flex" : "hover:text-gray-900 pt-1 hover:underline text-gray-600 w-[10rem] justify-center flex"}>
             <a href={href} {...props}>{children}</a>
         </li>
         </>
