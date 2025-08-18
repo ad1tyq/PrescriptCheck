03457439fecb0e6ad3fc698824252ad12bcafb86
Aayush Nikam <aayush.nikam217@gmail.com>
2025-08-19 00:27:56 +0530

    styled the website

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -2,11 +2,11 @@ function Navbar(){
     return(
         <>
         {/* navbar */}
-        <nav className="flex bg-gray-100 text-md px-10 py-5 border-0 border-b-1">
+        <nav className="flex justify-between bg-violet-400 text-md p-5">
             {/* logo */}
-            <a className="text-gray-900 text-2xl" href="/">ScriptCheck</a>
+            <a className="text-white font-semibold text-2xl" href="/">ScriptCheck</a>
             {/* navbar links */}
-            <ul className="flex ml-119">
+            <ul className="flex gap-10">
                 <CustomLink href="/upload">Upload Prescription</CustomLink>
                 <CustomLink href="/analysis">Medication Analysis</CustomLink>
                 <CustomLink href="/history">Medication History</CustomLink>
@@ -21,7 +21,7 @@ function CustomLink({href, children, ...props}){
         <>
         {/* navbar link component */}
         {/* if active ? css1 : css2 (for every other link) */}
-        <li className={path === href ? "text-gray-900 w-[17rem] hover:underline justify-center pt-1 flex" : "hover:text-gray-900 pt-1 hover:underline text-gray-600 w-[17rem] justify-center flex"}>
+        <li className={path === href ? "text-white font-bold duration-50 w-[10rem] justify-center pt-1 flex" : "text-white hover:font-bold pt-1 duration-50 w-[10rem] justify-center flex"}>
             <a href={href} {...props}>{children}</a>
         </li>
         </>
--- frontend/src/components/testimonialCard.jsx
+++ frontend/src/components/testimonialCard.jsx
@@ -5,10 +5,10 @@ const TestimonialCard = ({image, review}) => {
     // individual card
     return(
         <>
-        <div className="min-w-[30rem] h-50 border">
+        <div className="min-w-[30rem] rounded-lg overflow-y-scroll h-50 bg-gray-100 p-5 pt-3" style={{scrollbarWidth:"none"}}>
             <img src={image} alt="user testimonial"
-            className="h-15 w-15 rounded-[50px] object-cover"/>
-            <h3>{review}</h3>
+            className="h-15 ml-5 my-4 w-15 rounded-[50px] object-cover"/>
+            <h3 className="mx-4 mt-2">{review}</h3>
         </div>
         </>
     )
@@ -19,9 +19,9 @@ function TestCardContainer(){
         <>
         {/* entire component */}
         <div className="flex flex-col">
-            <h1 className="text-xl">User Testimonials</h1>
+            <h1 className="text-lg ml-10 -mb-5 text-[clamp(1.5rem,2vw,2rem)] font-semibold">User Testimonials</h1>
             {/* card container */}
-            <div className="flex overflow-x-scroll w-[100%] gap-10 py-10 px-5 bg-gray-200"
+            <div className="flex mx-4 rounded-lg overflow-x-scroll w-[97%] gap-10 py-10 px-5"
             style={{scrollbarWidth: 'none'}}>
                 {/* mapping array data for each card */}
                 {UserTestimonials.map((card) => (
