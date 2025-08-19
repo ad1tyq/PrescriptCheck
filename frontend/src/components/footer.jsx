458ae09000387be1b467cc310ea2477cb9218654
Aayush Nikam <aayush.nikam217@gmail.com>
2025-08-19 12:46:58 +0530

    footer edited

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,8 +1,16 @@
 function Footer(){
     return(
         <>
-        <div className="absolute w-[100%] h-[15%] bottom-0 bg-violet-100 justify-baseline">
-            footer
+        <div className="absolute w-[100%] h-[15%] bottom-0 bg-violet-400 flex flex-wrap justify-between">
+            <div className="flex-auto text-white mt-6 ml-10">
+                <a href="/" className="font-semibold mb-3 text-xl">About ScriptCheck</a>
+                <p>ScriptCheck is dedicated to providing users with the most reliable prescription management tools. Our mission is to ensure medication safety and efficiency.</p>
+            </div>
+            <div className="flex-auto mt-6 text-white">
+                <h1 className="font-semibold text-lg">Contact Us</h1>
+                <p>Email: support@prescriptcheck.com</p>
+                <p>Phone: +1 234 567 890</p>
+            </div>
         </div>
         </>
     )
