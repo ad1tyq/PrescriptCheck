b03fff5cba201f08441e8e5ec22c6bef2ddd2d3e
ad1tyq <adityabhardwaj9b7@gmail.com>
2025-08-22 03:49:58 +0530

    disclaimer

--- frontend/src/app/home/page.jsx
+++ frontend/src/app/home/page.jsx
@@ -1,11 +1,13 @@
 import Welcome from "../../components/welcome";
 import TestCardContainer from "../../components/testimonialCard";
+import Disclaimer from "../../components/disclaimer";
 function HomePage(){
     return(
         <>
-        <div className="h-[70vh]">
+        <div className="min-h-[70vh]">
             <Welcome/>
             <TestCardContainer/>
+            <Disclaimer/>
         </div>
         </>
     )
