fce162425e2183af63bb0c730ba1e57fbb7146a2
ad1tyq <adityabhardwaj9b7@gmail.com>
2025-08-22 18:21:44 +0530

    mobile view take 4

--- frontend/src/components/drugInteractCard.jsx
+++ frontend/src/components/drugInteractCard.jsx
@@ -18,11 +18,11 @@ function DrugInteractInfo() {
                 <h1 className="text-xl ml-1 -mt-1 font-semibold ">Drug Interactions</h1>
                 <div className="flex flex-col gap-5">
 
-                    <div className="bg-red-300 flex flex-col gap-5 p-3 pb-6 rounded-lg shadow-lg">
-                        <div className="flex justify-center w-[21rem] md:ml-[0.1rem] -ml-[3.5rem] mt-2 gap-5">
+                    <div className="bg-red-300 flex flex-col justify-center gap-5 p-3 px-1 pb-6 rounded-lg shadow-lg">
+                        <div className="flex w-[100%] md:ml-[1.2rem] ml-[0.9rem] mt-2 gap-[5%] md:gap-4">
                             <img src="../../assets/radioactive.svg" alt="high risk" className="h-8 w-8" />
                             <h1 className="text-xl md:mt-[3px] font-semibold hidden md:block text-[clamp(0.9rem,1.2vw,1.2rem)]">High Risk Interaction Detected</h1>
-                            <h1 className="text-xl md:mt-[1px] font-semibold md:hidden text-[clamp(0.9rem,1.2vw,1.2rem)]">High Risk
+                            <h1 className="text-xl md:mt-[1px] font-semibold md:hidden text-[clamp(0.8rem,1.2vw,1.2rem)]">High Risk
                                 <br/>Interaction Detected</h1>
                         </div>
                         {drugInteractInfo.HighRisk.length > 0 ? (
@@ -35,21 +35,21 @@ function DrugInteractInfo() {
                                 )
                             })
                         ) : (
-                            <div key={`no-warnings`} className="ml-5 text-[clamp(0.9rem,1.1vw,1.1rem)]">
+                            <div key={`no-warnings`} className="ml-5 text-[clamp(0.8rem,1.1vw,1.1rem)]">
                                 <b>No Warnings</b>
                             </div>
                         )}
                     </div>
 
                     <div className="bg-amber-100 flex flex-col gap-5 p-3 pb-6 rounded-lg shadow-lg">
-                        <div className="flex justify-center w-[13rem] md:ml-[0.5rem] mt-2 gap-5">
+                        <div className="flex w-[100%] md:ml-[1rem] ml-[0.5rem] mt-2 gap-[5%] md:gap-4">
                             <img src="../../assets/exclamation-circle-fill.svg" alt="high risk" className="h-8 w-8" />
                             <h1 className="text-xl md:mt-[1px] mt-1 font-semibold text-[clamp(0.9rem,1.2vw,1.2rem)]">Dosage Warning</h1>
                         </div>
                         {drugInteractInfo.DosageWarning.length > 0 ? (
                             drugInteractInfo.DosageWarning.map((dosage, index) => {
                                 return (
-                                    <div key={`dose${index}`} className="md:ml-5 mx-4 text-[clamp(0.9rem,1.1vw,1.1rem)]">
+                                    <div key={`dose${index}`} className="md:ml-5 mx-4 text-[clamp(0.8rem,1.1vw,1.1rem)]">
                                         <b>{dosage.med}:</b> {dosage.message}
                                     </div>
                                 )
